9cb9ab4d075827407c7bc04f3a0a17db02c94952
unknown <85491405+pantsutabs@users.noreply.github.com>
2022-09-06 21:38:48 +0300

    scuffed contracts v1

---
+++ contracts/ScuffedFemboys.sol
@@ -0,0 +1,85 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+//import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+
+contract ScuffedFemboys is ERC721Enumerable, Ownable, ReentrancyGuard, ERC2981  {
+
+    string public baseURI;
+
+    // Mint params
+    address public saleReceiver; // sale receiver is also the royalty receiver
+    uint256 public buyPrice = 0.05 ether;
+    uint256 public maxScuffies4Sale;
+    uint256 public scuffiesSold = 0;
+
+    uint256 public maxScuffies4Claim;
+    uint256 public scuffiesClaimed = 0;
+    uint256 public scuffiesClaimCount = 2;
+    bytes32 public claimRoot;
+    mapping(address => bool) public claimedAddresses;
+
+    constructor(string memory name_, string memory symbol_, uint256 maxSupplySale, uint256 maxSupplyClaim, address saleReceiver_) Ownable() ERC721(name_, symbol_) {
+        saleReceiver = saleReceiver_;
+        _setDefaultRoyalty(saleReceiver, 400); // 4%
+        maxScuffies4Sale = maxSupplySale;
+        maxScuffies4Claim = maxSupplyClaim;
+    }
+
+    function setBaseURI(string memory newURI) public onlyOwner {
+        baseURI = newURI;
+    }
+
+    // Overwrite
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+    
+    function setClaimRoot(bytes32 newClaimRoot) public onlyOwner {
+        claimRoot = newClaimRoot;
+    }
+
+    // Claim free pair
+    function claim(bytes32[] calldata _merkleProof) external nonReentrant {
+        require(claimedAddresses[msg.sender] == false, "Claimed already");
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(MerkleProof.verify(_merkleProof, claimRoot, leaf), "Incorrect proof");
+        require(scuffiesClaimed + scuffiesClaimCount <= maxScuffies4Claim, "Not enough left to claim");
+
+        scuffiesClaimed += scuffiesClaimCount;
+        claimedAddresses[msg.sender] = true;
+
+        uint supply = totalSupply();
+        for (uint256 i = 0; i < scuffiesClaimCount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+    }
+
+    // Buy 1-100
+    function buy(uint256 count) external nonReentrant payable  {
+        require(count > 0, "Cannot mint 0");
+        require(count <= 100, "What are you doing");
+        require(scuffiesSold + count <= maxScuffies4Sale, "Not enough left for sale");
+        require(msg.value == count * buyPrice, "Wrong ETH sum sent");
+        
+        scuffiesSold += count;
+
+        uint supply = totalSupply();
+        for (uint256 i = 0; i < count; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+    }
+
+    function withdrawETH() external /* onlyOwner */ {
+        payable(saleReceiver).transfer(address(this).balance);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Enumerable, ERC2981) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ test/tests.js
@@ -0,0 +1,145 @@
+const { expect } = require("chai");
+const { waffle } = require("hardhat");
+const hre = require("hardhat");
+
+const {MerkleTree} = require("merkletreejs")
+const keccak256 = require("keccak256")
+
+describe("test everything", function() {
+  	it("works probably as intended", async function() {
+     	this.timeout(20000);
+		const provider = waffle.provider;
+		const [owner, addr1, addr2, addr3, addr4, addr5, addr6] = await hre.ethers.getSigners();
+		const addrOwner = await owner.getAddress(); // deployer
+		const addr1raw = await addr1.getAddress(); // sale receiver
+		const addr2raw = await addr2.getAddress(); // put on claim list
+		const addr3raw = await addr3.getAddress(); // put on claim list
+		const addr4raw = await addr4.getAddress(); // put on claim list
+		const addr5raw = await addr5.getAddress(); // normal buyer
+		const addr6raw = await addr6.getAddress(); // normal buyer
+		const ScuffedFemboys = await hre.ethers.getContractFactory("ScuffedFemboys");
+		const ScuffedFemboys1 = await ScuffedFemboys.deploy("Scuffed Femboys", "SCUFFED", 3, 4, addr1raw);
+
+		await ScuffedFemboys1.deployed();
+
+		// Set up the merkle tree
+		let addresses = [
+			addr2raw,
+			addr3raw,
+			addr4raw
+		];
+		// Hash addresses to get the leaves
+		let leaves = addresses.map(addr => keccak256(addr))
+		// Create tree
+		let merkleTree = new MerkleTree(leaves, keccak256, {sortPairs: true})
+		// Get root
+		let rootHash = merkleTree.getRoot();//.toString('hex')
+		// Pretty-print tree
+		console.log(merkleTree.toString());
+		console.log("root:", rootHash);
+
+		// (owner) set root
+		await ScuffedFemboys1.connect(owner).setClaimRoot(rootHash);
+		
+		// (addr2) mint a free pair with bad verification - fail
+		{
+			let address = addr3raw; // uses addr3's hashes for addr2
+			let hashedAddress = keccak256(address);
+			let proof = merkleTree.getHexProof(hashedAddress);
+			await expect(ScuffedFemboys1.connect(addr2).claim(proof)).to.be.revertedWith('Incorrect proof');
+		}
+
+		// (addr2) mint a free pair with good verification - success
+		{
+			let address = addr2raw;
+			let hashedAddress = keccak256(address);
+			let proof = merkleTree.getHexProof(hashedAddress);
+			await ScuffedFemboys1.connect(addr2).claim(proof);
+		}
+
+		// (addr2) try to repeat the previous - (already claimed) - fail
+		{
+			let address = addr2raw;
+			let hashedAddress = keccak256(address);
+			let proof = merkleTree.getHexProof(hashedAddress);
+			await expect(ScuffedFemboys1.connect(addr2).claim(proof)).to.be.revertedWith('Claimed already');
+		}
+
+		// (addr3) mint a free pair with good verification - success
+		{
+			let address = addr3raw;
+			let hashedAddress = keccak256(address);
+			let proof = merkleTree.getHexProof(hashedAddress);
+			await ScuffedFemboys1.connect(addr3).claim(proof);
+		}
+
+		// (addr4) mint a free pair with good verification - (no claims left total) - fail 
+		{
+			let address = addr4raw;
+			let hashedAddress = keccak256(address);
+			let proof = merkleTree.getHexProof(hashedAddress);
+			await expect(ScuffedFemboys1.connect(addr4).claim(proof)).to.be.revertedWith('Not enough left to claim');
+		}
+
+		// (addr5) buy one - with no/less money - fail
+		await expect(ScuffedFemboys1.connect(addr5).buy(1)).to.be.revertedWith('Wrong ETH sum sent');
+		await expect(ScuffedFemboys1.connect(addr5).buy(1, {value: hre.ethers.utils.parseEther("0.00")})).to.be.revertedWith('Wrong ETH sum sent');
+		await expect(ScuffedFemboys1.connect(addr5).buy(1, {value: hre.ethers.utils.parseEther("0.02")})).to.be.revertedWith('Wrong ETH sum sent');
+
+		// (addr5) buy one - with more money - fail
+		await expect(ScuffedFemboys1.connect(addr5).buy(1, {value: hre.ethers.utils.parseEther("0.06")})).to.be.revertedWith('Wrong ETH sum sent');
+		await expect(ScuffedFemboys1.connect(addr5).buy(1, {value: hre.ethers.utils.parseEther("1.06")})).to.be.revertedWith('Wrong ETH sum sent');
+		
+		// (addr5) buy 0 or too many - fail
+		await expect(ScuffedFemboys1.connect(addr5).buy(0, {value: hre.ethers.utils.parseEther("0.05")})).to.be.revertedWith('Cannot mint 0');
+		await expect(ScuffedFemboys1.connect(addr5).buy(0, {value: hre.ethers.utils.parseEther("0.00")})).to.be.revertedWith('Cannot mint 0');
+		await expect(ScuffedFemboys1.connect(addr5).buy(150, {value: hre.ethers.utils.parseEther("7.5")})).to.be.revertedWith('What are you doing');
+
+		// (addr5) buy one - with accurate money - success
+		await ScuffedFemboys1.connect(addr5).buy(1, {value: hre.ethers.utils.parseEther("0.05")});
+
+		// (addr6) buy the remaining 2 together - success
+		await ScuffedFemboys1.connect(addr6).buy(2, {value: hre.ethers.utils.parseEther("0.1")});
+
+		// (addr5) buy one - with accurate money - (no mints left) - fail
+		await expect(ScuffedFemboys1.connect(addr5).buy(1, {value: hre.ethers.utils.parseEther("0.05")})).to.be.revertedWith('Not enough left for sale');
+
+		// (addr6) buy one - with accurate money - (no mints left) - fail
+		await expect(ScuffedFemboys1.connect(addr6).buy(1, {value: hre.ethers.utils.parseEther("0.05")})).to.be.revertedWith('Not enough left for sale');
+
+		// (addr2) buy one - with accurate money - (no mints left) - fail
+		await expect(ScuffedFemboys1.connect(addr2).buy(1, {value: hre.ethers.utils.parseEther("0.05")})).to.be.revertedWith('Not enough left for sale');
+
+		// (addr6) transfer id:5 to addr5 - success
+		await ScuffedFemboys1.connect(addr6).transferFrom(addr6raw, addr5raw, 5);
+
+		// (addr6) transfer id:5 to addr2 - (doesn't own) - fail
+		await expect(ScuffedFemboys1.connect(addr6).transferFrom(addr6raw, addr2raw, 5)).to.be.revertedWith('ERC721: caller is not token owner nor approved');
+
+		// (addr6) transfer id:3 to addr5 - (doesn't own) - fail
+		await expect(ScuffedFemboys1.connect(addr6).transferFrom(addr6raw, addr5raw, 3)).to.be.revertedWith('ERC721: caller is not token owner nor approved');
+
+		// check balances, addr2 - 2, addr3 - 2, addr4 - 0, addr5 - 2, addr6 - 1
+		await expect(await ScuffedFemboys1.balanceOf(addr2raw)).to.equal(hre.ethers.BigNumber.from("2"));
+		await expect(await ScuffedFemboys1.balanceOf(addr3raw)).to.equal(hre.ethers.BigNumber.from("2"));
+		await expect(await ScuffedFemboys1.balanceOf(addr4raw)).to.equal(hre.ethers.BigNumber.from("0"));
+		await expect(await ScuffedFemboys1.balanceOf(addr5raw)).to.equal(hre.ethers.BigNumber.from("2"));
+		await expect(await ScuffedFemboys1.balanceOf(addr6raw)).to.equal(hre.ethers.BigNumber.from("1"));
+
+		// Non owner tries to set restricted parameters
+		await expect(ScuffedFemboys1.connect(addr3).setClaimRoot(rootHash)).to.be.revertedWith('Ownable: caller is not the owner');
+		await expect(ScuffedFemboys1.connect(addr3).setBaseURI("ipfs://testtesttest/")).to.be.revertedWith('Ownable: caller is not the owner');
+
+		// set URI
+		await ScuffedFemboys1.connect(owner).setBaseURI("ipfs://testtesttest/");
+		
+		// check the get URI that it works
+		await expect(await ScuffedFemboys1.connect(addr5).tokenURI(1)).to.equal("ipfs://testtesttest/1");
+
+		// (addr5) call withdraw ETH
+		await ScuffedFemboys1.connect(addr5).withdrawETH();
+
+		// check that the eth is received into (addr1)
+		expect(hre.ethers.utils.formatEther((await provider.getBalance(addr1raw)))).to.equal('10000.15');
+  	});
+});
